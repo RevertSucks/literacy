444ca9074cf6e73b08ccbdb531735ce7cd766611
RevertSucks <71314753+RevertSucks@users.noreply.github.com>
2021-11-04 02:49:41 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 13822889 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/RevertSucks/literacy/main/games/Lumber.lua?token=ARAC2QK4BFQYYJVKNWC26ZLBQOFJS", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RevertSucks/literacy/main/games/Lumber.lua", true))()
 end
