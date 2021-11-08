92362952ba663d7b868fb801e2785363265e2e4f
RevertSucks <71314753+RevertSucks@users.noreply.github.com>
2021-11-08 01:41:41 -0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 13822889 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RevertSucks/literacy/main/games/Lumber.lua", true))()
+elseif game.PlaceId == 734159876 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RevertSucks/literacy/main/games/Sharkbite.lua", true))()
 end
