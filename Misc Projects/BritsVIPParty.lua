6f08e8efaacacea91381b38084009cbd43584de2
RevertSucks <71314753+RevertSucks@users.noreply.github.com>
2022-02-11 06:32:49 -0600

    Update BritsVIPParty.lua

--- Misc Projects/BritsVIPParty.lua
+++ Misc Projects/BritsVIPParty.lua
@@ -1,12 +1,21 @@
---[[
-game link: https://www.roblox.com/games/8656125900/Brits-VIP-Party-ft-PinkPantheress
-]]--
-
 --collects all coins
+local meshes = {}
+local collectlimit = 25 --math.huge for inf
+
 for i,v in pairs(Workspace:GetChildren()) do
-    if v.Name == "Coin" then
-        wait(.1)
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame 
+   if v:IsA("MeshPart") then
+        table.insert(meshes, v.Name)
+   end
+end
+
+for i,v in pairs(meshes) do
+wait(.1)
+    if i < collectlimit then
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace[v].CFrame
+        table.remove(meshes, i)
+        print(i)
+    else
+        break
     end
 end
 
