f2e26a78e71d0131f83c9e5854534025284bc1a0
RevertSucks <71314753+RevertSucks@users.noreply.github.com>
2022-02-12 08:33:08 -0600

    Update Alo Sanctuary.lua

--- Misc Projects/Alo Sanctuary.lua
+++ Misc Projects/Alo Sanctuary.lua
@@ -1,17 +1,24 @@
-for i,v in pairs(game:GetService("Workspace").Map.Orbs:GetChildren()) do
-    wait(.2)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Part.CFrame
+for i,v in pairs(Workspace.Map.Orbs:GetChildren()) do
+    game:GetService("ReplicatedStorage").NetManagedInstances.orbCollection:FireServer(tostring(v))
 end
+--collects orbs
 
-for i,v in pairs(game:GetService("Workspace").YogaPoseStations:GetChildren()) do
-    wait(.5)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
-end
+game:GetService("ReplicatedStorage").NetManagedInstances.UnlockPose:FireServer("WarriorI")
+game:GetService("ReplicatedStorage").NetManagedInstances.UnlockPose:FireServer("WarriorII")
+game:GetService("ReplicatedStorage").NetManagedInstances.UnlockPose:FireServer("TreePose")
+game:GetService("ReplicatedStorage").NetManagedInstances.UnlockPose:FireServer("MountainPose")
+game:GetService("ReplicatedStorage").NetManagedInstances.UnlockPose:FireServer("LotusPose")
+game:GetService("ReplicatedStorage").NetManagedInstances.UnlockPose:FireServer("HighLunge")
+game:GetService("ReplicatedStorage").NetManagedInstances.UnlockPose:FireServer("TrianglePose")
+--collects poses
+
+
 game:GetService("ReplicatedStorage").NetManagedInstances.UnlockMat:FireServer("Black")
 game:GetService("ReplicatedStorage").NetManagedInstances.UnlockMat:FireServer("Pink")
 game:GetService("ReplicatedStorage").NetManagedInstances.UnlockMat:FireServer("Yellow")
 game:GetService("ReplicatedStorage").NetManagedInstances.UnlockMat:FireServer("LightGreen")
 game:GetService("ReplicatedStorage").NetManagedInstances.UnlockMat:FireServer("DarkGreen")
+--collects mats
 
 local ohTable1 = {
 	["pose"] = "TreePose",
@@ -21,5 +28,3 @@ local ohTable1 = {
 game:GetService("ReplicatedStorage").NetManagedInstances.BeginMeditation:FireServer(ohTable1)
 wait(.1)
 game:GetService("ReplicatedStorage").NetManagedInstances.FinishMeditation:FireServer()
-
-game.Playesr.LocalPlayer:Kick("If you didnt get the hat re-execute")
