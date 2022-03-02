754012681bcd70dd7db75b0aecfb482f681497e9
RevertSucks <71314753+RevertSucks@users.noreply.github.com>
2022-03-02 04:59:59 -0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,4 +2,8 @@ if game.PlaceId == 13822889 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RevertSucks/literacy/main/games/Lumber.lua", true))()
 elseif game.PlaceId == 734159876 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RevertSucks/literacy/main/games/Sharkbite.lua", true))()
+elseif game.PlaceId == 6055959032 then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/RevertSucks/literacy/main/games/jailbird/lobby.lua'))()
+elseif game.PlaceId == 6452089755 then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/RevertSucks/literacy/main/games/jailbird/playing.lua'))()
 end
