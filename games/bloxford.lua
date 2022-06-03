b3c8a16bacc4f9ed5f39467eb976f8191d336979
RevertSucks <71314753+RevertSucks@users.noreply.github.com>
2022-06-03 11:15:04 -0500

    Started ReWork

--- games/bloxford.lua
+++ games/bloxford.lua
@@ -1,1 +1,139 @@
+local gamename = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+local uiversion = "V.Alpha"
 
+
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/teppyboy/RbxScripts/master/Misc/UI_Libraries/Zypher/Library.lua"))()
+local esp = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
+
+--init
+local suc,er = pcall(function()
+    workspace.Animals.Goat:Destroy()
+end)
+
+function destroyui()
+    for i,v in pairs(game.CoreGui:GetChildren()) do
+        if v.name == tostring(gamename)..uiversion then
+            v:Destroy()
+            _G.mineaura = false
+            _G.hunting = false
+            _G.nonlr = false
+            _G.huntingauto = false
+            esp:Toggle(false)
+        end
+    end
+end
+destroyui()
+
+local main = library:CreateMain({
+	projName = tostring(gamename)..uiversion,
+	Resizable = true,
+	MinSize = UDim2.new(0,400,0,400),
+	MaxSize = UDim2.new(0,750,0,500),
+})
+
+--//functions\\--
+function teleport(time,cframe)
+    local Time = time
+    local CFrameEnd = cframe
+    local tween =  game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(Time), {CFrame = CFrameEnd})
+    tween:Play()
+    tween.Completed:Wait()    
+end
+function huntdeer()
+    for i,v in pairs(workspace.Animals:GetChildren()) do
+        game:GetService("ReplicatedStorage").Events.Hunt:FireServer(v)
+    end
+end
+
+--//locals\\--
+local minetable = {
+	[1] = "Mine",
+	[2] = "E"
+}
+local processtable = {
+	[1] = "Process Ores",
+	[2] = "E"
+}
+local destroytbl = {
+	[1] = "Destroy",
+	[2] = "E"
+}
+local pickuptbl = {
+	[1] = "Pick Up",
+	[2] = "E"
+}
+local cooktbl = {
+	[1] = "Cook Raw Meat",
+	[2] = "E"
+}
+local guttingtbl = {
+	[1] = "Gut Deer",
+	[2] = "E"
+}
+
+--//variables\\--
+_G.mineaura = false
+_G.hunting = false
+_G.nonlr = false
+_G.huntingauto = false
+local teletime = 10
+
+
+
+local Jobs = main:CreateCategory("Jobs")
+local ESPCat = main:CreateCategory("ESP")
+local TeleportMenu = main:CreateCategory("Teleports")
+local Misc = main:CreateCategory("Misc")
+
+
+local Miner = Jobs:CreateSection("Miner")
+local Hunter = Jobs:CreateSection("Hunter")
+local Police = Jobs:CreateSection("Police")
+
+local Teleports = TeleportMenu:CreateSection("Yurrr")
+
+local ESPSettings = ESPCat:CreateSection("Settings")
+local ESPTypes = ESPCat:CreateSection("ESP Types")
+
+local MiscCat = Misc:CreateSection("Misc")
+
+
+ESPSettings:Create("Toggle","Toggle ESP",function(state)
+    esp:Toggle(state)
+end,
+    {
+        default = false,
+    }
+)
+ESPSettings:Create("Toggle","Toggle Names",function(state)
+    esp.Names = state
+end,
+    {
+        default = true,
+    }
+)
+ESPSettings:Create("Toggle","Toggle Boxes",function(state)
+    esp.Boxes = state
+end,
+    {
+        default = true,
+    }
+)
+ESPSettings:Create("Toggle","Toggle Boxes Face Cam",function(state)
+    esp.FaceCamera = state
+end,
+    {
+        default = false,
+    }
+)
+
+
+MiscCat:Create("Button","Close UI",function()
+    destroyui()
+end,
+    {
+        animated = true,
+    }
+)
+
+MiscCat:Create("Textlabel",tostring(gamename).."\n"..uiversion)
