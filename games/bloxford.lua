22f29e531aee6e20518c12e291f21161cd658dc7
RevertSucks <71314753+RevertSucks@users.noreply.github.com>
2022-06-04 13:04:34 -0500

    finished UI moving

--- games/bloxford.lua
+++ games/bloxford.lua
@@ -76,7 +76,8 @@ _G.mineaura = false
 _G.hunting = false
 _G.nonlr = false
 _G.huntingauto = false
-local teletime = 10
+local teletime = 15
+local hidetype = true
 
 
 
@@ -97,6 +98,210 @@ local ESPTypes = ESPCat:CreateSection("ESP Types")
 
 local MiscCat = Misc:CreateSection("Misc")
 
+Miner:Create("Toggle","Mining Aura *SHIT*",function(state)
+
+    _G.mineaura = toggle
+
+    while _G.mineaura == true do
+        for _,v in pairs(game:GetService("Workspace").Map["Mining Rocks"]:GetChildren()) do
+            if v.mine.health.Value ~= 0 then
+                game:GetService("ReplicatedStorage").Events.ActionHandler2:FireServer(v.mine, minetable)
+                wait(0.01)
+            end
+        end
+    end
+
+end,
+    {
+        default = false,
+    }
+)
+
+Miner:Create("DropDown","Sell Ore", function(current)
+    game:GetService("ReplicatedStorage").Events.Trader2:FireServer(current)
+end,
+    {
+        options = {"Emerald","Platinum Bar","Ruby","Gold Ore","Rock","Diamond","Gold Bar","Iron Bar","Copper Bar"},
+        default = "Rock",
+        search = true
+    }
+)
+
+Miner:Create("Button","Sell All Ores",function()
+    game:GetService("ReplicatedStorage").Events.Trader2:FireServer("Emerald")
+    game:GetService("ReplicatedStorage").Events.Trader2:FireServer("Platinum Bar")
+    game:GetService("ReplicatedStorage").Events.Trader2:FireServer("Ruby")
+    game:GetService("ReplicatedStorage").Events.Trader2:FireServer("Gold Ore")
+    game:GetService("ReplicatedStorage").Events.Trader2:FireServer("Rock")
+    game:GetService("ReplicatedStorage").Events.Trader2:FireServer("Diamond")
+    game:GetService("ReplicatedStorage").Events.Trader2:FireServer("Gold Bar")
+    game:GetService("ReplicatedStorage").Events.Trader2:FireServer("Iron Bar")
+    game:GetService("ReplicatedStorage").Events.Trader2:FireServer("Copper Bar")
+end,
+    {
+        animated = true,
+    }
+)
+
+Miner:Create("Button","Smelt Ores",function()
+    for _,v in pairs(workspace:GetDescendants()) do
+        if v.Name == "smelter" then
+            game:GetService("ReplicatedStorage").Events.ActionHandler2:FireServer(v, processtable)
+            break
+        end
+    end
+end,
+    {
+        animated = true,
+    }
+)
+
+Hunter:Create("Toggle","Auto Hunt *MANUAL GUTTING*",function(state)
+
+    _G.hunting = toggle
+
+    while _G.hunting == true do
+        huntdeer()
+        wait(1)
+    end
+
+end,
+    {
+        default = false,
+    }
+)
+
+Hunter:Create("Toggle","Auto Hunt *MUST BE AT CAMPFIRE*",function(state)
+
+    _G.huntingauto = toggle
+
+    while _G.huntingauto == true do
+
+        for i,v in pairs(workspace.Animals:GetChildren()) do
+            game:GetService("ReplicatedStorage").Events.Hunt:FireServer(v)
+            wait(.5)
+            v.CanCollide = false
+            v.CFrame = CFrame.new(873.347, 30.5709, -1685.36)
+            wait(.5)
+            game:GetService("ReplicatedStorage").Events.ActionHandler2:FireServer(v.deer, guttingtbl)
+        end
+
+
+        for i,v in pairs(workspace:GetChildren()) do
+            if v.Name == "Handle" and string.find(v.droppeditem.itemName.Value,"Meat") then
+                v.CFrame = CFrame.new(873.347, 28.5709, -1685.36)
+                wait(.3)
+                game:GetService("ReplicatedStorage").Events.ActionHandler2:FireServer(v.droppeditem, pickuptbl)
+            end
+        end
+        wait(.5)
+
+        for i,v in pairs(workspace.Map.CampfireModel:GetDescendants()) do
+            if v.Name == "campfire" then
+                game:GetService("ReplicatedStorage").Events.ActionHandler2:FireServer(v, cooktbl)
+            end
+        end
+
+        wait(3.5)
+
+        for i,v in pairs(workspace:GetChildren()) do
+            if v.Name == "Handle" and string.find(v.droppeditem.itemName.Value,"Cooked") then
+                game:GetService("ReplicatedStorage").Events.ActionHandler2:FireServer(v.droppeditem, pickuptbl)
+            end
+        end
+        wait(.2)
+        game:GetService("ReplicatedStorage").Events.Trader2:FireServer("Cooked Meat")
+
+    end
+
+end,
+    {
+        default = false,
+    }
+)
+
+Hunter:Create("Button","Hunt Deer *MANUAL GUTTING*",function()
+    huntdeer()
+end,
+    {
+        animated = true,
+    }
+)
+
+Hunter:Create("Button","Sell Meat *MUST BE COOKED*",function()
+    game:GetService("ReplicatedStorage").Events.Trader2:FireServer("Cooked Meat")
+end,
+    {
+        animated = true,
+    }
+)
+
+Police:Create("Button","Destroy All Printers",function()
+    for i,v in pairs(workspace.Entities:GetChildren()) do
+        if string.find(v.name,"Printer") then
+            game:GetService("ReplicatedStorage").Events.ActionHandler2:FireServer(v.Main.moneyprinter, destroytbl)
+        end
+    end
+end,
+    {
+        animated = true,
+    }
+)
+
+
+Teleports:Create("Slider","Teleport Speed *PUT HIGHER IF FAR AWAY*",function(value)
+    teletime = value
+end,
+    {
+        min = 5,
+        max = 60,
+        default = 15,
+        changablevalue = true
+    }
+)
+
+Teleports:Create("Button","Campfire",function()
+    teleport(teletime,CFrame.new(873.347, 28.5709, -1685.36))
+end,
+    {
+        animated = true,
+    }
+)
+
+Teleports:Create("Button","Spawn",function()
+    teleport(teletime,CFrame.new(473.065, 13.4125, -220.866))
+end,
+    {
+        animated = true,
+    }
+)
+
+Teleports:Create("Button","Suburbs",function()
+    teleport(teletime,CFrame.new(355.742, 13.7437, -557.904))
+end,
+    {
+        animated = true,
+    }
+)
+
+Teleports:Create("Button","Desert",function()
+    teleport(teletime,CFrame.new(1888.87, 21.9031, -164.663))
+end,
+    {
+        animated = true,
+    }
+)
+
+Teleports:Create("DropDown","PlayerTeleport", function(current)
+    if current ~= game.Players.LocalPlayer.Name then
+        teleport(teletime,CFrame.new(workspace[current].HumanoidRootPart.Position))
+    end
+end,
+    {
+        playerlist = true,
+        search = true
+    }
+)
 
 ESPSettings:Create("Toggle","Toggle ESP",function(state)
     esp:Toggle(state)
@@ -126,6 +331,8 @@ end,
         default = false,
     }
 )
+
+--printer esp
 esp:AddObjectListener(game:GetService("Workspace").Entities, {
     Color =  Color3.new(1,1,0),
     Type = "Model",
@@ -160,6 +367,7 @@ game:GetService("Workspace").Entities.ChildAdded:Connect(function(obj)
     IsEnabled = "Items"
     })
 end)
+--shipment esp
 esp:AddObjectListener(game:GetService("Workspace").Entities, {
     Name = "Shipment",
     CustomName = function(itm)
@@ -172,7 +380,7 @@ game:GetService("Workspace").Entities.ChildAdded:Connect(function(obj)
     esp:Add(obj, {
         Name = "Shipment",
         CustomName = function(itm)
-            return itm.shipment.type.Value.." Shipment"
+            return itm:FindFirstChildOfClass("IntValue").type.Value.." Shipment"
         end,
         Color = Color3.fromRGB(255, 255, 0),
         IsEnabled = "Items"
@@ -183,7 +391,7 @@ ESPTypes:Create("Toggle","Players",function(state)
     esp.Players = state
 end,
     {
-        default = false,
+        default = true,
     }
 )
 
@@ -195,6 +403,38 @@ end,
     }
 )
 
+MiscCat:Create("KeyBind","Hide Menu", function()
+    for i,v in pairs(game.CoreGui:GetChildren()) do
+        if v.name == tostring(gamename)..uiversion then
+            if hidetype == true then
+                hidetype = false
+                v.Enabled = hidetype
+            elseif hidetype == false then
+                hidetype = true
+                v.Enabled = hidetype
+            end
+        end
+    end
+end,
+    {
+        default = Enum.KeyCode.N
+    }
+)
+
+MiscCat:Create("Button","Grab Crates",function()
+    for i,v in pairs(workspace:GetChildren()) do
+        if v.Name == "CrateSpawner" and v:FindFirstChild("Crate") then
+            teleport(15,v.Crate.CFrame)
+            v.Crate.CanCollide = false
+            wait(.1)
+            game:GetService("ReplicatedStorage").Events.ActionHandler2:FireServer(v.Crate.lootcrate, pickuptbl)
+        end
+    end
+end,
+    {
+        animated = true,
+    }
+)
 
 MiscCat:Create("Button","Close UI",function()
     destroyui()
